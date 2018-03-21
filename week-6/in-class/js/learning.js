ed1a6f3faed701498a3c99b38cc49184809aa732
chivvis <chivvis@simmons.edu>
2018-03-20 20:48:09 -0400

    First JavaScript in-class assignment

--- week-6/in-class/js/learning.js
+++ week-6/in-class/js/learning.js
@@ -1,2 +1,44 @@
 // "use strict" tells the browser to enforce some rules about what can be in our JavaScript.
 "use strict";
+
+let my_name = "Daniel";
+let your_name = "Caligula";
+
+if (your_name === "Daniel") {
+  renderOutput("That's me!");
+} else if (your_name === "Caligula") {
+  renderOutput("That's Caligula!");
+}
+else {
+  renderOutput("I must be someone else");
+}
+
+function helloWorld(){
+  renderOutput("Hello World");
+}
+
+helloWorld();
+helloWorld();
+helloWorld();
+
+function helloName(name) {
+  renderOutput("Hello " + name);
+}
+
+helloName("Daniel");
+helloName("Caligula");
+helloName("Martha");
+
+function todaysHours(one_days_hours) {
+  renderOutput(one_days_hours);
+}
+
+//todaysHours("Tuesday 9a - 5p");
+
+hours.forEach(todaysHours);
+
+
+
+
+
+
